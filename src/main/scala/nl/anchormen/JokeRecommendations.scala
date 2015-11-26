962cdeef4b52bec3e2ad10d3a8c0fca9d8ed6780
Jeroen Vlek <j.vlek@anchormen.nl>
2015-11-26 15:48:13 +0100

    Fixed dataset loading

--- src/main/scala/nl/anchormen/JokeRecommendations.scala
+++ src/main/scala/nl/anchormen/JokeRecommendations.scala
@@ -6,6 +6,8 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.Row
 import org.apache.spark.rdd.RDD
+import org.apache.spark.ml.recommendation.ALS
+import scala.util.Try
 
 /**
  * @author Jeroen Vlek <j.vlek@anchormen.nl>
@@ -15,29 +17,40 @@ object JokeRecommendations {
   def main(args : Array[String]) {
     assert(args.size > 0, "Please supply the (hdfs) path to the Jester dataset")
     val path = args(0)
-    
-    val sparkConf = new SparkConf()
+
+   // Set up
+   val sparkConf = new SparkConf()
       .setAppName("Joke Recommendations")
-  
    val sparkContext = new SparkContext(sparkConf)
    val sqlContext = new SQLContext(sparkContext)
    
+   // Get data in right format
    val ratings = loadRatings(sqlContext, path)
+   
+   // Train model
+   val alsEstimator = new ALS
+   val model = alsEstimator.fit(ratings)
    
    sparkContext.stop()
   }
   
   def loadRatings(sqlContext : SQLContext, path : String) : DataFrame = {
 	  import com.databricks.spark.csv._
-    val ratingsMatrix = sqlContext.csvFile(path)
+    val ratingsMatrix = sqlContext.csvFile(path, false)
     
-    // we need to flatten the Dataframe from {user_id, rating_item1, rating_item2, rating_item3...} to {user, item, rating} 
-    val flatRatingsRdd = ratingsMatrix.flatMap( { row =>
-        (1 to row.size - 1).map(itemIndex => (row.getString(0), itemIndex, row.getDouble(itemIndex)))
+    // just generate user ids, this can also be used for generating primary keys
+    val withUserIds = ratingsMatrix.rdd.zipWithUniqueId
+    
+    // we need to reduce the dimensionality of the matrix 
+    // from {numJokes, rating_item1, rating_item2, rating_item3...} 
+    // to {user, item, rating} 
+    val flatRatingsRdd = withUserIds.flatMap( {
+      case (row : Row, userId : Long) =>
+        (1 to row.size - 1).map(itemIndex => Try(userId, itemIndex, row.getString(itemIndex).toDouble).toOption).flatten
     })
       
     import sqlContext.implicits._
-    flatRatingsRdd.toDF("user", "item", "rating")
+    flatRatingsRdd.toDF("user", "item", "rating").where($"rating" < 99)
   }
   
 }
