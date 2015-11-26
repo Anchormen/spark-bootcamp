5504e7c76744683596eaebb0ada880d6bef0a8d8
Jeroen Vlek <j.vlek@anchormen.nl>
2015-11-26 11:48:31 +0100

    set up and teardown SparkContext

--- src/main/scala/nl/anchormen/JokeRecommendations.scala
+++ src/main/scala/nl/anchormen/JokeRecommendations.scala
@@ -1,6 +1,7 @@
 package nl.anchormen
 
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 
 /**
  * @author Jeroen Vlek <j.vlek@anchormen.nl>
@@ -8,7 +9,14 @@ import org.apache.spark.SparkContext
 object JokeRecommendations {
   
   def main(args : Array[String]) {
-    assert(args.size > 1, "Please supply the (hdfs) path to the Jester dataset")
+    assert(args.size > 0, "Please supply the (hdfs) path to the Jester dataset")
+    
+    val sparkConf = new SparkConf()
+      .setAppName("Joke Recommendations")
+  
+   val sparkContext = new SparkContext(sparkConf)
+    
+   sparkContext.stop()
   }
   
 }
